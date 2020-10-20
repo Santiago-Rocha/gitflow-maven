46223c71db5a4e96c90f67d69f9e62e3e3b00afe
santiago-rocha <santiago.rocha@payulatam.com>
2020-10-20 12:52:14 -0500

    Init project

---
+++ src/test/java/com/example/gitflowplugin/GitflowPluginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitflowplugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitflowPluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
