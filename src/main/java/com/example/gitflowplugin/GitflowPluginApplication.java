20d80c1ed71cd7b56292076d2f4607bd63414ba7
santiago-rocha <santiago.rocha@payulatam.com>
2020-10-20 14:57:32 -0500

    Added new functions

--- src/main/java/com/example/gitflowplugin/GitflowPluginApplication.java
+++ src/main/java/com/example/gitflowplugin/GitflowPluginApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitflowPluginApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Esto es una prueba");
 		SpringApplication.run(GitflowPluginApplication.class, args);
 	}
 
